7b2d416a51f5f07c7f97446e0a8f08a277d41895
tiffanyjoa8 <46694937+tiffanyjoa8@users.noreply.github.com>
2019-03-03 01:03:29 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -12,17 +12,10 @@ function hte(){
 }
 function hello1(){
     console.log("fsdgrhj");
-    // document.getElementById('heh').innerHTML+=' <img src="https://lh3.googleusercontent.com/dDmd7s-dzlZ1SY7bfjIUSY9jmNKMEf01ONynseHrvstWuX8jLlGJe5ziEZHW77H3xKhWUOMffz6qEETRnG2tDV3TjcA4xbrjzzT-cc0OLHFr-1OeWosvJON78Eyk-SxxuI4fvpSsz1cK67Jm_OlCtc4ButJwK50fRbdytri7OwXwOBHMko1-dklzJUrT2oGUGqh_udY0GwnQBqLytKoRwNvopX2ark4tc23PN1Cshoy7yVh2jmFCyvZNvoDXeqOXnwN73c9XqfLqGUiIu7y-qaZyCh_avDe2sg8NPDg1y8CTDvEPXOnOX8khFHY44ptFkFDmZrdrKu-oR18cEgMCeBFsCpqK-2mlXdgkyYZCUVa7YBR04FezE7tjTajXC740T3H0kg_UDvM2GWJpdHdGWVKgtTe4a4SrCQTCcE3S2R-349E4LB4y2HMBvG0gD9O75OLWIZv0cKH4sgxDXxzTa-OsXELDFCx9cb417C9eq4DY5Z4IF5_9wqCErTByV2sSXKig1cV21MXxlfooSuUxp8Zrt_RKlGabhAZ-u64QkbxmwJBRbFQlU9l_KK7fvggsOgyaUsdp_LBUw9Lrft5KoOTAoQljCv25OXO_jmj393UqLM05TJeXynIVu31EKxnznRPGb0IrQhP9bv34VM9-HLTIOIsBL5M=w1204-h903-no" style="width:1370px;height:300px;">';
-    // document.getElementById('heh2').innerHTML+='The potato is coming out of his cage and is doing just fine :)';
-
     
     var text = "The potato is coming out of his cage and doing just fine :)";
-    // <output> text</output>
+
     alert(text);
-    // return text;
-    // text;
-    // <output>'The potato is coming out of his cage and is doing just fine'</output> 
-    // document.getElementById('heh').innerHTML+= data;
 }
 function hello2(){
     console.log("fsdgrhj");
@@ -34,7 +27,7 @@ function hello2(){
 function hello3(){
     console.log("fsdgrhj");
     
-    var text = "The potato is named Juan after our CS professor. ";
+    var text = "The potato is dreaming about world peace, sunsets, and long walks on the beach. ";
     alert(text);
 }
 
@@ -50,7 +43,6 @@ function hello4(){
 function hhte2(){
     console.log("fsdgrhj");
     document.getElementById('heh').innerHTML+=' <img src="https://i.pinimg.com/564x/4d/a1/8c/4da18c43deb0d7f0bde7430e76ea5b96.jpg">';
-    // document.getElementById('heh').innerHTML+= data;
 
 }
 
