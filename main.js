0f17a3b0a1f5b896221005e0e844e02e39cca316
tiffanyjoa8 <46694937+tiffanyjoa8@users.noreply.github.com>
2019-03-02 22:02:32 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let data;
 
 function hte(){
     console.log("fsdgrhj");
-    fetch('https://api.fitbit.com/1/user/7CT2RJ/activities/heart/date/today/1d.json')
+    fetch('')
     .then(function(resp){
         return resp.json();
     }).then(function(resp){
@@ -10,11 +10,49 @@ function hte(){
 
     })
 }
-function hhte(){
+function hello1(){
     console.log("fsdgrhj");
-    document.getElementById('heh').innerHTML+=' <img src="https://cdn-image.foodandwine.com/sites/default/files/styles/medium_2x/public/1536255525/mcdonalds-french-fries-FT-BLOG0818.jpg?itok=xbCzN-Xb">';
-    document.getElementById('heh').innerHTML+= data;
+    // document.getElementById('heh').innerHTML+=' <img src="https://lh3.googleusercontent.com/dDmd7s-dzlZ1SY7bfjIUSY9jmNKMEf01ONynseHrvstWuX8jLlGJe5ziEZHW77H3xKhWUOMffz6qEETRnG2tDV3TjcA4xbrjzzT-cc0OLHFr-1OeWosvJON78Eyk-SxxuI4fvpSsz1cK67Jm_OlCtc4ButJwK50fRbdytri7OwXwOBHMko1-dklzJUrT2oGUGqh_udY0GwnQBqLytKoRwNvopX2ark4tc23PN1Cshoy7yVh2jmFCyvZNvoDXeqOXnwN73c9XqfLqGUiIu7y-qaZyCh_avDe2sg8NPDg1y8CTDvEPXOnOX8khFHY44ptFkFDmZrdrKu-oR18cEgMCeBFsCpqK-2mlXdgkyYZCUVa7YBR04FezE7tjTajXC740T3H0kg_UDvM2GWJpdHdGWVKgtTe4a4SrCQTCcE3S2R-349E4LB4y2HMBvG0gD9O75OLWIZv0cKH4sgxDXxzTa-OsXELDFCx9cb417C9eq4DY5Z4IF5_9wqCErTByV2sSXKig1cV21MXxlfooSuUxp8Zrt_RKlGabhAZ-u64QkbxmwJBRbFQlU9l_KK7fvggsOgyaUsdp_LBUw9Lrft5KoOTAoQljCv25OXO_jmj393UqLM05TJeXynIVu31EKxnznRPGb0IrQhP9bv34VM9-HLTIOIsBL5M=w1204-h903-no" style="width:1370px;height:300px;">';
+    // document.getElementById('heh2').innerHTML+='The potato is coming out of his cage and is doing just fine :)';
+
+    
+    var text = "The potato is coming out of his cage and doing just fine :)";
+    // <output> text</output>
+    alert(text);
+    // return text;
+    // text;
+    // <output>'The potato is coming out of his cage and is doing just fine'</output> 
+    // document.getElementById('heh').innerHTML+= data;
+}
+function hello2(){
+    console.log("fsdgrhj");
+    
+    var text = "The potato is sleeping. Shhh";
+    alert(text);
+}
+
+function hello3(){
+    console.log("fsdgrhj");
+    
+    var text = "The potato is named Juan after our CS professor. ";
+    alert(text);
+}
+
+function hello4(){
+    console.log("fsdgrhj");
+    
+    var text = "Yes!";
+    alert(text);
+}
+
+
+
+function hhte2(){
+    console.log("fsdgrhj");
+    document.getElementById('heh').innerHTML+=' <img src="https://i.pinimg.com/564x/4d/a1/8c/4da18c43deb0d7f0bde7430e76ea5b96.jpg">';
+    // document.getElementById('heh').innerHTML+= data;
 
 }
+
 
 
